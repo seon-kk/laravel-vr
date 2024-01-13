3984d5f6b77092777292189fee419deca73733fb
seon-kk <seon_kk@naver.com>
2024-01-13 20:07:03 +0900

    Feat: API 경로 라우터 설정

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('/v1')->get('/how-to-lose-weight');
