9e25a90b4804460107a948a10df486971b1050c9
seon-kk <seon_kk@naver.com>
2024-01-13 20:22:20 +0900

    Feat: 라이프스타일 태그 Enum 생성

---
+++ app/Enums/LifeStyleEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * 라이프스타일 태그 Enum
+ */
+enum LifeStyleEnum: string
+{
+    case ENOUGH_MONEY = 'enough_money';
+    case STRONG_WILL = 'strong_will';
+    case ENOUGH_TIME = 'enough_time';
+}
