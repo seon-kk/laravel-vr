a72fd7fa314421798946df828a4e5bcfc96f1723
seon-kk <seon_kk@naver.com>
2024-01-14 00:12:21 +0900

    fix: 잘못 지정된 collection 클래스 수정

--- app/Services/FitnessCoach.php
+++ app/Services/FitnessCoach.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use App\Enums\FitnessSolutionEnum;
 use App\Enums\LifeStyleEnum;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 class FitnessCoach
 {
