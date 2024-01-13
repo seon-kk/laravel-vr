cc9b83029787e5196738a04155106312cc1c2d01
seon-kk <seon_kk@naver.com>
2024-01-13 21:06:04 +0900

    feat: PersonalTrainer 클래스 생성, ConsultingController 와 연결

--- app/Http/Controllers/ConsultingController.php
+++ app/Http/Controllers/ConsultingController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PersonalTrainer;
+
 class ConsultingController extends Controller
 {
+    public function __construct(
+        private PersonalTrainer $personalTrainer
+    )
+    {
+    }
+
     public function getWeightConsulting()
     {
-
+        $this->personalTrainer->getWeightConsulting();
     }
 }
---
+++ app/Services/PersonalTrainer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Collection;
+
+/**
+ * 요청 정보를 토대로 해당 전문가에게 솔루션 문의 후
+ * 전문가별 솔루션을 모아서 전달
+ */
+class PersonalTrainer
+{
+    private Collection $solutions;
+
+    public function __construct() {
+        $this->solutions = new Collection();
+    }
+
+    public function getWeightConsulting()
+    {
+
+    }
+}
