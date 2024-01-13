59279d86cc20f5edc4174a188ca045723695e59e
seon-kk <seon_kk@naver.com>
2024-01-13 23:18:31 +0900

    test: 성공 테스트에 리퀘스트 쿼리 추가

--- tests/Feature/ConsultingControllerTest.php
+++ tests/Feature/ConsultingControllerTest.php
@@ -2,18 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Enums\LifeStyleEnum;
+use App\Enums\SolutionTypeEnum;
 use Tests\TestCase;
 
 class ConsultingControllerTest extends TestCase
 {
     /**
-     * /v1/how-to-lose-weight 경로로 GET 요청 테스트
+     * /v1/how-to-lose-weight 경로로 GET 요청 성공 테스트
      *
      * @return void
      */
     public function test_weight_consult_ok()
     {
-        $response = $this->get('/v1/how-to-lose-weight');
+        $weightRequest = [
+            'life_style' => [LifeStyleEnum::STRONG_WILL->value, LifeStyleEnum::ENOUGH_MONEY->value],
+            'prefer_solution_type' => SolutionTypeEnum::DIET->value
+        ];
+
+        $response = $this->get('/v1/how-to-lose-weight?' . http_build_query($weightRequest));
 
         $response->assertStatus(200);
     }
