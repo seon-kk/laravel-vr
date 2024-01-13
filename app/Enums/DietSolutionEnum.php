e9e681ac7c532afb8dcbcc6e1b9004e44653ed55
seon-kk <seon_kk@naver.com>
2024-01-13 21:14:21 +0900

    feat: 다이어트 솔루션 종류 Enum 클래스

---
+++ app/Enums/DietSolutionEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * 다이어트 솔루션 종류 Enum
+ */
+enum DietSolutionEnum :string
+{
+    case INTERMITTENT_FASTING = 'Intermittent Fasting';
+    case LCHF = 'LCHF';
+}
