a94e0c4618708cbbaa44b8d2f38d6ffbba8ad254
seon-kk <seon_kk@naver.com>
2024-01-13 22:57:04 +0900

    feat: enum value 값을 array로 리턴 (request validation 용도)

--- app/Enums/LifeStyleEnum.php
+++ app/Enums/LifeStyleEnum.php
@@ -10,4 +10,20 @@ enum LifeStyleEnum: string
     case ENOUGH_MONEY = 'enough_money';
     case STRONG_WILL = 'strong_will';
     case ENOUGH_TIME = 'enough_time';
+
+
+    /**
+     * 전체 Enum value 값을 array로 리턴
+     * @return array
+     */
+    public static function getValueArray(): array
+    {
+        $valueArray = [];
+
+        foreach(self::cases() as $lifeStyleEnum) {
+            $valueArray[] = $lifeStyleEnum->value;
+        }
+
+        return $valueArray;
+    }
 }
