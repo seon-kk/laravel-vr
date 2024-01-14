326b8c9de92fe19cf31b5287544267c8e84fb081
seon-kk <seon_kk@naver.com>
2024-01-14 11:13:48 +0900

    feat: PersonalTrainer - 순서대로 각 솔루션 조회 후 목록 합치기

--- app/Services/PersonalTrainer.php
+++ app/Services/PersonalTrainer.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Enums\SolutionTypeEnum;
 use Illuminate\Support\Collection;
 
 /**
@@ -12,12 +13,45 @@ class PersonalTrainer
 {
     private Collection $solutions;
 
-    public function __construct() {
+    public function __construct(
+        private DietExpert   $dietExpert,
+        private FitnessCoach $fitnessCoach
+    )
+    {
         $this->solutions = new Collection();
     }
 
-    public function getWeightConsulting(array $lifeStyles, string $preferSolutionType = null)
-    {
 
+    public function getWeightConsulting(array $lifeStyles, string $preferSolutionType = null): Collection
+    {
+        if($preferSolutionType == SolutionTypeEnum::DIET->value || $preferSolutionType == null){
+            $this->addDietSolution($lifeStyles);
+            $this->addFitnessSolution($lifeStyles);
+        } else {
+            $this->addFitnessSolution($lifeStyles);
+            $this->addDietSolution($lifeStyles);
+        }
+
+        return $this->solutions;
+    }
+
+
+    private function addDietSolution(array $lifeStyles): void
+    {
+        $dietSolution = $this->dietExpert->getRecommendSolutions($lifeStyles);
+        $this->mergeSolutions($dietSolution);
+    }
+
+
+    private function addFitnessSolution(array $lifeStyles): void
+    {
+        $fitnessSolution = $this->fitnessCoach->getRecommendSolutions($lifeStyles);
+        $this->mergeSolutions($fitnessSolution);
+    }
+
+    
+    private function mergeSolutions(Collection $solution): void
+    {
+        $this->solutions = $this->solutions->merge($solution);
     }
 }
